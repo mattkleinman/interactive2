d7d0a667b1584aea48c3f808b33d25cb7023dd87
m k <mattkleinman@Matts-MacBook-Pro.local>
2017-09-11 13:10:00 -0700

    asdad

---
+++ 50/assets/script.js
@@ -0,0 +1,261 @@
+$(document).ready(function(){
+	
+	$('#button-3').click(function() {
+		$('#button-2').animate({
+			opacity: 0.4,
+			marginLeft: '0.6in',
+			fontSize: '24px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+	$('#button-4').mouseover(function() {
+		$(this).animate({
+			opacity: 0.9,
+			marginLeft: '0.6in',
+			fontSize: '24px',
+			borderWidth: '20px'
+		}, 1000 );
+	});
+
+	$('#button-6').mouseover(function() {
+		$(this).animate({
+			opacity: 0.9,
+			marginRight: '0.6in',
+			fontSize: '24px',
+			borderWidth: '20px'
+		}, 4000 );
+	});
+
+		$('#button-7').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '0.2in',
+			fontSize: '24px',
+			borderWidth: '40px'
+		}, 2000 );
+
+			$('#button-14').mouseover(function() {
+		$(this).animate({
+			opacity: 0.5,
+			marginLeft: '1.3in',
+			fontSize: '24px',
+			borderWidth: '20px'
+		}, 1500 );
+	});
+	});
+		$('#button-20').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '2.3in',
+			fontSize: '24px',
+			borderWidth: '20px'
+		}, 2500 );
+	});
+			$('#button-10').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '4.3in',
+			fontSize: '24px',
+			borderWidth: '40px'
+		}, 3500 );
+	});
+
+				$('#button-12').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '3.3in',
+			fontSize: '24px',
+			borderWidth: '40px'
+		}, 100 );
+	});
+			$('#button-5').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '3.3in',
+			fontSize: '24px',
+			borderWidth: '-40px'
+		}, 100 );
+	});
+
+	$('#button-9').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '8.3in',
+			fontSize: '24px',
+			borderWidth: '20px'
+		}, 500 );
+	});
+
+	$('#button-20').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '8.3in',
+			fontSize: '24px',
+			borderWidth: '10px'
+		}, 500 );
+	});
+
+	$('#button-14').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '8.3in',
+			fontSize: '24px',
+			borderWidth: '20px'
+		}, 500 );
+	});
+
+	$('#button-24').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '9.3in',
+			fontSize: '24px',
+			borderWidth: '50px'
+		}, 600 );
+	});
+
+	$('#button-25').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '6.3in',
+			fontSize: '24px',
+			borderWidth: '50px'
+		}, 600 );
+	});
+
+	$('#button-1').mouseover(function() {
+		$(this).animate({
+			opacity: 0.3,
+			marginLeft: '6.3in',
+			fontSize: '24px',
+			borderWidth: '50px'
+		}, 600 );
+	});
+
+	$('#button-17').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '58px'
+		}, 600 );
+	});
+
+		$('#button-18').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '58px'
+		}, 600 );
+	});
+
+		$('#button-50').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '58px'
+		}, 600 );
+	});
+
+		$('#button-48').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '58px'
+		}, 600 );
+	});
+
+		$('#button-42').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '70px'
+		}, 600 );
+	});
+		$('#button-37').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '70px'
+		}, 600 );
+	});
+
+		$('#button-33').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '70px'
+		}, 600 );
+	});
+
+
+		$('#button-26').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '79px'
+		}, 1000 );
+	});
+
+$('#button-41').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '79px'
+		}, 1000 );
+	});
+	
+	$('#button-34').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '79px'
+		}, 1000 );
+	});
+
+	$('#button-32').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '79px'
+		}, 1000 );
+	});
+
+	$('#button-37').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '79px'
+		}, 1000 );
+	});
+
+	$('#button-22').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '79px'
+		}, 1000 );
+	});
+
+	$('#button-38').mouseover(function() {
+		$(this).animate({
+			opacity: 0.6,
+			marginLeft: '5.6in',
+			fontSize: '22px',
+			borderWidth: '79px'
+		}, 1000 );
+	});
+
+});
